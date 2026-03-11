817d408933f330858870a1a6adddc7ecf6bd6998
Aelin Reidel <aelin@mainlining.org>
2026-03-11 22:00:12 +0100

    WIP: drm: panel: panel-visionox-r66451: hacks

--- drivers/gpu/drm/panel/panel-visionox-r66451.c
+++ drivers/gpu/drm/panel/panel-visionox-r66451.c
@@ -1,30 +1,37 @@
-//SPDX-License-Identifier: GPL-2.0-only
-//Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (c) 2026 FIXME
+// Generated with linux-mdss-dsi-panel-driver-generator from vendor device tree:
+//   Copyright (c) 2013, The Linux Foundation. All rights reserved. (FIXME)
 
 #include <linux/backlight.h>
 #include <linux/delay.h>
 #include <linux/gpio/consumer.h>
+#include <linux/mod_devicetable.h>
 #include <linux/module.h>
-#include <linux/of.h>
 #include <linux/regulator/consumer.h>
 
-#include <drm/drm_mipi_dsi.h>
-#include <drm/drm_probe_helper.h>
-#include <drm/drm_modes.h>
-#include <drm/drm_panel.h>
 #include <drm/display/drm_dsc.h>
 #include <drm/display/drm_dsc_helper.h>
-
-#include <video/mipi_display.h>
+#include <drm/drm_mipi_dsi.h>
+#include <drm/drm_modes.h>
+#include <drm/drm_panel.h>
+#include <drm/drm_probe_helper.h>
 
 struct visionox_r66451 {
 	struct drm_panel panel;
 	struct mipi_dsi_device *dsi;
+	struct drm_dsc_config dsc;
+	struct regulator_bulk_data *supplies;
 	struct gpio_desc *reset_gpio;
-	struct regulator_bulk_data supplies[2];
 };
 
-static inline struct visionox_r66451 *to_visionox_r66451(struct drm_panel *panel)
+static const struct regulator_bulk_data visionox_r66451_supplies[] = {
+	{ .supply = "vddio" },
+	{ .supply = "vdd" },
+};
+
+static inline
+struct visionox_r66451 *to_visionox_r66451(struct drm_panel *panel)
 {
 	return container_of(panel, struct visionox_r66451, panel);
 }
@@ -32,111 +39,137 @@ static inline struct visionox_r66451 *to_visionox_r66451(struct drm_panel *panel
 static void visionox_r66451_reset(struct visionox_r66451 *ctx)
 {
 	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
-	usleep_range(10000, 10100);
+	usleep_range(10000, 11000);
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-	usleep_range(10000, 10100);
+	usleep_range(10000, 11000);
 	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
-	usleep_range(10000, 10100);
+	usleep_range(10000, 11000);
 }
 
 static int visionox_r66451_on(struct visionox_r66451 *ctx)
 {
-	struct mipi_dsi_device *dsi = ctx->dsi;
-	struct mipi_dsi_multi_context dsi_ctx = { .dsi = dsi };
+	struct mipi_dsi_multi_context dsi_ctx = { .dsi = ctx->dsi };
 
-	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
+	ctx->dsi->mode_flags |= MIPI_DSI_MODE_LPM;
 
-	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xb0, 0x00);
-	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xc2,
-				     0x09, 0x24, 0x0c, 0x00, 0x00, 0x0c, 0x00, 0x00, 0x00,
-				     0x09, 0x3c);
-	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xd7,
-				     0x00, 0xb9, 0x3c, 0x00, 0x40, 0x04, 0x00, 0xa0, 0x0a,
-				     0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x19,
-				     0x3c, 0x00, 0x40, 0x04, 0x00, 0xa0, 0x0a);
-	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xb0, 0x80);
-	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xde,
-				     0x40, 0x00, 0x18, 0x00, 0x18, 0x00, 0x18, 0x00, 0x18,
-				     0x10, 0x00, 0x18, 0x00, 0x18, 0x00, 0x18, 0x02, 0x00, 0x00);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xb0, 0x04);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xe8, 0x00, 0x02);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xe4, 0x00, 0x08);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xb4, 0x20, 0x1c);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xb6,
+				     0x6c, 0x00, 0x06, 0x23, 0xaf, 0x13, 0x1a,
+				     0x05, 0x04, 0xfa, 0x05, 0x20);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xb0, 0x00);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xc4,
-				     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-				     0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x32);
-	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xcf,
-				     0x64, 0x0b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08,
-				     0x00, 0x0b, 0x77, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
-				     0x02, 0x02, 0x02, 0x02, 0x02, 0x03);
+				     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				     0x00, 0x10, 0x00, 0x00, 0x02, 0x00, 0x00,
+				     0x00, 0x29, 0x00, 0x01, 0x00, 0x00, 0x00,
+				     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				     0x00, 0x22, 0x00, 0x00, 0x00, 0x00, 0x11,
+				     0x00, 0x00, 0x0c, 0x00, 0x00, 0x00, 0x00,
+				     0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xd0,
+				     0x44, 0x44, 0xb2, 0x28, 0x00, 0x28, 0x5a,
+				     0x00, 0x5a, 0x03, 0x0d, 0x01);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xd3,
-				     0x45, 0x00, 0x00, 0x01, 0x13, 0x15, 0x00, 0x15, 0x07,
-				     0x0f, 0x77, 0x77, 0x77, 0x37, 0xb2, 0x11, 0x00, 0xa0,
-				     0x3c, 0x9c);
-	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xd7,
-				     0x00, 0xb9, 0x34, 0x00, 0x40, 0x04, 0x00, 0xa0, 0x0a,
-				     0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x19,
-				     0x34, 0x00, 0x40, 0x04, 0x00, 0xa0, 0x0a);
+				     0x49, 0x00, 0x00, 0x01, 0x1a, 0x15, 0x00,
+				     0x15, 0x07, 0x0f, 0x77, 0x77, 0x77, 0x37,
+				     0xb2, 0x11, 0x00, 0xa0, 0x3c, 0x9a);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xd8,
-				     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-				     0x3a, 0x00, 0x3a, 0x00, 0x3a, 0x00, 0x3a, 0x00, 0x3a,
-				     0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-				     0x00, 0x0a, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x00, 0x00,
-				     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a,
-				     0x00, 0x32, 0x00, 0x0a, 0x00, 0x22);
+				     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				     0x00, 0x00, 0x30, 0x00, 0x30, 0x00, 0x30,
+				     0x00, 0x30, 0x00, 0x30, 0x05, 0x00, 0x00,
+				     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				     0x0f, 0x00, 0x0f, 0x00, 0x00, 0x00, 0x00,
+				     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				     0x00, 0x00, 0x0f, 0x00, 0x2f, 0x00, 0x0f,
+				     0x00, 0x20);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xdf,
-				     0x50, 0x42, 0x58, 0x81, 0x2d, 0x00, 0x00, 0x00, 0x00,
-				     0x00, 0x00, 0x6b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-				     0x00, 0x00, 0x01, 0x0f, 0xff, 0xd4, 0x0e, 0x00, 0x00,
-				     0x00, 0x00, 0x00, 0x00, 0x0f, 0x53, 0xf1, 0x00, 0x00,
-				     0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+				     0x50, 0x42, 0x58, 0x81, 0x2d, 0x00, 0x00,
+				     0x00, 0x00, 0x00, 0x00, 0x6b, 0x00, 0x00,
+				     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+				     0x0f, 0xff, 0xd4, 0x0e, 0x00, 0x00, 0x00,
+				     0x00, 0x00, 0x00, 0x0f, 0x53, 0x18, 0x00,
+				     0x0f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xeb, 0x8b, 0x8b);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf7, 0x01);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xb0, 0x80);
-	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xe4, 0x34, 0xb4, 0x00, 0x00, 0x00, 0x39,
-				     0x04, 0x09, 0x34);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xe4,
+				     0x34, 0xb4, 0x00, 0x00, 0x00, 0x30, 0x04,
+				     0x0c, 0xe2);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xe6, 0x00);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xb0, 0x04);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xdf, 0x50, 0x40);
-	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf3, 0x50, 0x00, 0x00, 0x00, 0x00);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf3,
+				     0x50, 0x00, 0x00, 0x00, 0x00);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf2, 0x11);
-	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf3, 0x01, 0x00, 0x00, 0x00, 0x01);
+	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf3,
+				     0x01, 0x00, 0x00, 0x00, 0x01);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf4, 0x00, 0x02);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xf2, 0x19);
 	mipi_dsi_dcs_write_seq_multi(&dsi_ctx, 0xdf, 0x50, 0x42);
 	mipi_dsi_dcs_set_tear_on_multi(&dsi_ctx, MIPI_DSI_DCS_TEAR_MODE_VBLANK);
-	mipi_dsi_dcs_set_column_address_multi(&dsi_ctx, 0, 1080 - 1);
-	mipi_dsi_dcs_set_page_address_multi(&dsi_ctx, 0, 2340 - 1);
-
-	dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
+	mipi_dsi_dcs_set_column_address_multi(&dsi_ctx, 0x0000, 1080 - 1);
+	mipi_dsi_dcs_set_page_address_multi(&dsi_ctx, 0x0000, 2340 - 1);
+	mipi_dsi_dcs_exit_sleep_mode_multi(&dsi_ctx);
+	mipi_dsi_msleep(&dsi_ctx, 120);
+	mipi_dsi_dcs_set_display_on_multi(&dsi_ctx);
 
 	return dsi_ctx.accum_err;
 }
 
-static void visionox_r66451_off(struct visionox_r66451 *ctx)
+static int visionox_r66451_off(struct visionox_r66451 *ctx)
 {
+	struct mipi_dsi_multi_context dsi_ctx = { .dsi = ctx->dsi };
+
 	ctx->dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
+
+	mipi_dsi_dcs_set_display_off_multi(&dsi_ctx);
+	mipi_dsi_msleep(&dsi_ctx, 20);
+	mipi_dsi_dcs_enter_sleep_mode_multi(&dsi_ctx);
+	mipi_dsi_msleep(&dsi_ctx, 120);
+
+	return dsi_ctx.accum_err;
 }
 
 static int visionox_r66451_prepare(struct drm_panel *panel)
 {
 	struct visionox_r66451 *ctx = to_visionox_r66451(panel);
+	struct device *dev = &ctx->dsi->dev;
+	struct drm_dsc_picture_parameter_set pps;
 	int ret;
 
-	ret = regulator_bulk_enable(ARRAY_SIZE(ctx->supplies),
-				    ctx->supplies);
-	if (ret < 0)
+	ret = regulator_bulk_enable(ARRAY_SIZE(visionox_r66451_supplies), ctx->supplies);
+	if (ret < 0) {
+		dev_err(dev, "Failed to enable regulators: %d\n", ret);
 		return ret;
+	}
 
 	visionox_r66451_reset(ctx);
 
 	ret = visionox_r66451_on(ctx);
 	if (ret < 0) {
+		dev_err(dev, "Failed to initialize panel: %d\n", ret);
 		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-		regulator_bulk_disable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
+		regulator_bulk_disable(ARRAY_SIZE(visionox_r66451_supplies), ctx->supplies);
 		return ret;
 	}
 
-	mipi_dsi_compression_mode(ctx->dsi, true);
+	drm_dsc_pps_payload_pack(&pps, &ctx->dsc);
+
+	ret = mipi_dsi_picture_parameter_set(ctx->dsi, &pps);
+	if (ret < 0) {
+		dev_err(panel->dev, "failed to transmit PPS: %d\n", ret);
+		return ret;
+	}
+
+	ret = mipi_dsi_compression_mode(ctx->dsi, true);
+	if (ret < 0) {
+		dev_err(dev, "failed to enable compression mode: %d\n", ret);
+		return ret;
+	}
+
+	msleep(28); /* TODO: Is this panel-dependent? */
 
 	return 0;
 }
@@ -144,94 +177,85 @@ static int visionox_r66451_prepare(struct drm_panel *panel)
 static int visionox_r66451_unprepare(struct drm_panel *panel)
 {
 	struct visionox_r66451 *ctx = to_visionox_r66451(panel);
+	struct device *dev = &ctx->dsi->dev;
+	int ret;
 
-	visionox_r66451_off(ctx);
+	ret = visionox_r66451_off(ctx);
+	if (ret < 0)
+		dev_err(dev, "Failed to un-initialize panel: %d\n", ret);
 
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-	regulator_bulk_disable(ARRAY_SIZE(ctx->supplies), ctx->supplies);
+	regulator_bulk_disable(ARRAY_SIZE(visionox_r66451_supplies), ctx->supplies);
 
 	return 0;
 }
 
 static const struct drm_display_mode visionox_r66451_mode = {
-	.clock = 345830,
+	.clock = (1080 + 96 + 32 + 40) * (2340 + 25 + 1 + 4) * 144 / 1000,
 	.hdisplay = 1080,
-	.hsync_start = 1175,
-	.hsync_end = 1176,
-	.htotal = 1216,
+	.hsync_start = 1080 + 96,
+	.hsync_end = 1080 + 96 + 32,
+	.htotal = 1080 + 96 + 32 + 40,
 	.vdisplay = 2340,
-	.vsync_start = 2365,
-	.vsync_end = 2366,
-	.vtotal = 2370,
+	.vsync_start = 2340 + 25,
+	.vsync_end = 2340 + 25 + 1,
+	.vtotal = 2340 + 25 + 1 + 4,
 	.width_mm = 0,
 	.height_mm = 0,
 	.type = DRM_MODE_TYPE_DRIVER,
 };
 
-static int visionox_r66451_enable(struct drm_panel *panel)
-{
-	struct visionox_r66451 *ctx = to_visionox_r66451(panel);
-	struct mipi_dsi_device *dsi = ctx->dsi;
-	struct drm_dsc_picture_parameter_set pps;
-	struct mipi_dsi_multi_context dsi_ctx = { .dsi = dsi };
-
-	if (!dsi->dsc) {
-		dev_err(&dsi->dev, "DSC not attached to DSI\n");
-		return -ENODEV;
-	}
-
-	drm_dsc_pps_payload_pack(&pps, dsi->dsc);
-	mipi_dsi_picture_parameter_set_multi(&dsi_ctx, &pps);
-
-	mipi_dsi_dcs_exit_sleep_mode_multi(&dsi_ctx);
-	mipi_dsi_msleep(&dsi_ctx, 120);
-
-	mipi_dsi_dcs_set_display_on_multi(&dsi_ctx);
-	mipi_dsi_msleep(&dsi_ctx, 20);
-
-	return dsi_ctx.accum_err;
-}
-
-static int visionox_r66451_disable(struct drm_panel *panel)
-{
-	struct visionox_r66451 *ctx = to_visionox_r66451(panel);
-	struct mipi_dsi_device *dsi = ctx->dsi;
-	struct mipi_dsi_multi_context dsi_ctx = { .dsi = dsi };
-
-	mipi_dsi_dcs_set_display_off_multi(&dsi_ctx);
-	mipi_dsi_msleep(&dsi_ctx, 20);
-
-	mipi_dsi_dcs_enter_sleep_mode_multi(&dsi_ctx);
-	mipi_dsi_msleep(&dsi_ctx, 120);
-
-	return dsi_ctx.accum_err;
-}
-
 static int visionox_r66451_get_modes(struct drm_panel *panel,
-				    struct drm_connector *connector)
+					    struct drm_connector *connector)
 {
-	drm_connector_helper_get_modes_fixed(connector, &visionox_r66451_mode);
-	return 1;
+	return drm_connector_helper_get_modes_fixed(connector, &visionox_r66451_mode);
 }
 
-static const struct drm_panel_funcs visionox_r66451_funcs = {
+static const struct drm_panel_funcs visionox_r66451_panel_funcs = {
 	.prepare = visionox_r66451_prepare,
 	.unprepare = visionox_r66451_unprepare,
 	.get_modes = visionox_r66451_get_modes,
-	.enable = visionox_r66451_enable,
-	.disable = visionox_r66451_disable,
 };
 
 static int visionox_r66451_bl_update_status(struct backlight_device *bl)
 {
 	struct mipi_dsi_device *dsi = bl_get_data(bl);
 	u16 brightness = backlight_get_brightness(bl);
+	int ret;
 
-	return mipi_dsi_dcs_set_display_brightness(dsi, brightness);
+	dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
+
+	ret = mipi_dsi_dcs_set_display_brightness_large(dsi, brightness);
+	if (ret < 0)
+		return ret;
+
+	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
+
+	return 0;
+}
+
+// TODO: Check if /sys/class/backlight/.../actual_brightness actually returns
+// correct values. If not, remove this function.
+static int visionox_r66451_bl_get_brightness(struct backlight_device *bl)
+{
+	struct mipi_dsi_device *dsi = bl_get_data(bl);
+	u16 brightness;
+	int ret;
+
+	dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
+
+	ret = mipi_dsi_dcs_get_display_brightness_large(dsi, &brightness);
+	if (ret < 0)
+		return ret;
+
+	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
+
+	return brightness;
 }
 
 static const struct backlight_ops visionox_r66451_bl_ops = {
 	.update_status = visionox_r66451_bl_update_status,
+	.get_brightness = visionox_r66451_bl_get_brightness,
 };
 
 static struct backlight_device *
@@ -240,7 +264,7 @@ visionox_r66451_create_backlight(struct mipi_dsi_device *dsi)
 	struct device *dev = &dsi->dev;
 	const struct backlight_properties props = {
 		.type = BACKLIGHT_RAW,
-		.brightness = 255,
+		.brightness = 2048,
 		.max_brightness = 4095,
 	};
 
@@ -252,67 +276,67 @@ static int visionox_r66451_probe(struct mipi_dsi_device *dsi)
 {
 	struct device *dev = &dsi->dev;
 	struct visionox_r66451 *ctx;
-	struct drm_dsc_config *dsc;
-	int ret = 0;
+	int ret;
 
 	ctx = devm_drm_panel_alloc(dev, struct visionox_r66451, panel,
-				   &visionox_r66451_funcs,
+				   &visionox_r66451_panel_funcs,
 				   DRM_MODE_CONNECTOR_DSI);
 	if (IS_ERR(ctx))
 		return PTR_ERR(ctx);
 
-	dsc = devm_kzalloc(dev, sizeof(*dsc), GFP_KERNEL);
-	if (!dsc)
-		return -ENOMEM;
-
-	/* Set DSC params */
-	dsc->dsc_version_major = 0x1;
-	dsc->dsc_version_minor = 0x2;
-
-	dsc->slice_height = 20;
-	dsc->slice_width = 540;
-	dsc->slice_count = 2;
-	dsc->bits_per_component = 8;
-	dsc->bits_per_pixel = 8 << 4;
-	dsc->block_pred_enable = true;
-
-	dsi->dsc = dsc;
-
-	ctx->supplies[0].supply = "vddio";
-	ctx->supplies[1].supply = "vdd";
-
-	ret = devm_regulator_bulk_get(&dsi->dev, ARRAY_SIZE(ctx->supplies),
-			ctx->supplies);
-
+	ret = devm_regulator_bulk_get_const(dev,
+					    ARRAY_SIZE(visionox_r66451_supplies),
+					    visionox_r66451_supplies,
+					    &ctx->supplies);
 	if (ret < 0)
 		return ret;
 
-	ctx->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_LOW);
+	ctx->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(ctx->reset_gpio))
-		return dev_err_probe(dev, PTR_ERR(ctx->reset_gpio), "Failed to get reset-gpios\n");
+		return dev_err_probe(dev, PTR_ERR(ctx->reset_gpio),
+				     "Failed to get reset-gpios\n");
 
 	ctx->dsi = dsi;
 	mipi_dsi_set_drvdata(dsi, ctx);
 
-	dsi->lanes = 4;
+	dsi->lanes = 3;
 	dsi->format = MIPI_DSI_FMT_RGB888;
-	dsi->mode_flags = MIPI_DSI_MODE_LPM | MIPI_DSI_CLOCK_NON_CONTINUOUS;
+	dsi->mode_flags = MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_DSC_ALL_SLICES_IN_PKT;
+
 	ctx->panel.prepare_prev_first = true;
 
 	ctx->panel.backlight = visionox_r66451_create_backlight(dsi);
 	if (IS_ERR(ctx->panel.backlight))
 		return dev_err_probe(dev, PTR_ERR(ctx->panel.backlight),
-				"Failed to create backlight\n");
+				     "Failed to create backlight\n");
 
 	drm_panel_add(&ctx->panel);
 
+	/* This panel only supports DSC; unconditionally enable it */
+	dsi->dsc = &ctx->dsc;
+
+	ctx->dsc.dsc_version_major = 1;
+	ctx->dsc.dsc_version_minor = 1;
+
+	ctx->dsc.slice_height = 20;
+	ctx->dsc.slice_width = 540;
+	/*
+	 * TODO: hdisplay should be read from the selected mode once
+	 * it is passed back to drm_panel (in prepare?)
+	 */
+	WARN_ON(1080 % ctx->dsc.slice_width);
+	ctx->dsc.slice_count = 1080 / ctx->dsc.slice_width;
+	ctx->dsc.bits_per_component = 8;
+	ctx->dsc.bits_per_pixel = 8 << 4; /* 4 fractional bits */
+	ctx->dsc.block_pred_enable = true;
+
 	ret = mipi_dsi_attach(dsi);
 	if (ret < 0) {
-		dev_err(dev, "Failed to attach to DSI host: %d\n", ret);
 		drm_panel_remove(&ctx->panel);
+		return dev_err_probe(dev, ret, "Failed to attach to DSI host\n");
 	}
 
-	return ret;
+	return 0;
 }
 
 static void visionox_r66451_remove(struct mipi_dsi_device *dsi)
@@ -322,14 +346,14 @@ static void visionox_r66451_remove(struct mipi_dsi_device *dsi)
 
 	ret = mipi_dsi_detach(dsi);
 	if (ret < 0)
-		dev_err(&dsi->dev, "Failed to detach DSI host: %d\n", ret);
+		dev_err(&dsi->dev, "Failed to detach from DSI host: %d\n", ret);
 
 	drm_panel_remove(&ctx->panel);
 }
 
 static const struct of_device_id visionox_r66451_of_match[] = {
-	{.compatible = "visionox,r66451"},
-	{ /*sentinel*/ }
+	{ .compatible = "visionox,r66451" }, // FIXME
+	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, visionox_r66451_of_match);
 
@@ -337,13 +361,12 @@ static struct mipi_dsi_driver visionox_r66451_driver = {
 	.probe = visionox_r66451_probe,
 	.remove = visionox_r66451_remove,
 	.driver = {
-		.name = "panel-visionox-r66451",
+		.name = "panel-r66451-plus-144hz-cphy",
 		.of_match_table = visionox_r66451_of_match,
 	},
 };
-
 module_mipi_dsi_driver(visionox_r66451_driver);
 
-MODULE_AUTHOR("Jessica Zhang <quic_jesszhan@quicinc.com>");
-MODULE_DESCRIPTION("Panel driver for the Visionox R66451 AMOLED DSI panel");
+MODULE_AUTHOR("linux-mdss-dsi-panel-driver-generator <fix@me>"); // FIXME
+MODULE_DESCRIPTION("DRM driver for r66451 amoled cmd mode dsi visionox panel with DSC");
 MODULE_LICENSE("GPL");
